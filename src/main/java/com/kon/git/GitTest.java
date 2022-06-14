24ebec002983ce571ff566b5e03f21bfd5d97a21
lukai <lukaixxx@foxmail.com>
2022-06-14 13:40:57 +0800

    push commit

--- src/main/java/com/kon/git/GitTest.java
+++ src/main/java/com/kon/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello,git3");
         System.out.println("master test");
         System.out.println("hello,hot-fix");
+        System.out.println("push test!");
 
     }
 }
