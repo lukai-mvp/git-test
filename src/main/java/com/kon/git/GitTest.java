56d4c35cbd10301facd5518fedcbaf1297f445f9
lukai <lukaixxx@foxmail.com>
2022-06-14 12:10:04 +0800

    hot-fix commit2

--- src/main/java/com/kon/git/GitTest.java
+++ src/main/java/com/kon/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello,gitte");
         System.out.println("hello,github");
         System.out.println("hello,git3");
+        System.out.println("hello,hot-fix");
 
     }
 }
